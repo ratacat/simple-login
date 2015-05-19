419ba9dd7601517d406487a05ca6f8797acc4023
ratacat <neuralsplash@gmail.com>
2015-05-19 14:35:17 -0700

    installed sessions/cookie handler:

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    session = require('express-session'),
+    db = require('./models'),
     app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({
+	secret: 'super secret',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use("/", function (req, res, next) {
+
+  req.login = function (user) {
+    req.session.userId = user.id;
+  };
+
+  req.currentUser = function (cb) {
+     db.User.
+      findOne({
+          id: req.session.userId
+      },
+      function (err, user) {
+        req.user = user;
+        cb(null, user);
+      })
+  };
+
+  req.logout = function () {
+    req.session.userId = null;
+    req.user = null;
+  }
+
+  next(); 
+});
 
 app.get("/signup", function (req, res) {
   res.send("Coming soon");
 });
 
+app.post("/users", function(req,res) {
+	var user = req.body.user;
+
+	db.User.createSecure(user.email,user.password,function() {
+		res.send(user.email +"signed up!");
+	});
+});
+
+
 //is accepting a form post with creds and authenticate account
 app.post("/login", function (req, res) {
+	var user = req.body.user;
+
 	db.User.authenticate(user.email,user.password,
 		function(err,user) {
-			console.log("logging in");
+			console.log(user + "logging in");
 			res.send("logged in!");
 		});
 });
